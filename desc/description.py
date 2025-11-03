6a9d2f0a8ab6bb5f69cf77e25c6184ba401dbbc5
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-11-03 21:19:54 +0530

    intro

---
+++ desc/description.py
@@ -0,0 +1,51 @@
+#  What is Django?
+
+# Django is a high-level Python web framework used to build secure, scalable, and fast web applications.
+# It follows the Model-View-Template (MVT) architecture, helping developers build websites quickly without needing to reinvent basic components like authentication, database handling, or admin panels.
+
+#  Key Features of Django
+
+# Rapid Development – You can build web apps quickly with minimal code.
+
+# MVT Architecture – Helps in separating business logic, design, and data handling.
+
+# Built-in Admin Interface – Automatically generated admin dashboard for managing data.
+
+# ORM (Object-Relational Mapper) – Allows you to interact with databases using Python instead of SQL.
+
+# Security – Protects against common attacks like SQL injection, XSS, CSRF, etc.
+
+# Scalable & Reusable – Used by large applications like Instagram and Pinterest.
+
+# Open Source – Free and maintained by a large community
+
+
+
+
+#Step 1: Create a Virtual Environment
+
+#Open VS Code terminal (or Command Prompt / PowerShell) and run:
+#
+#python -m venv venv
+# Step 2: Activate the Virtual Environment
+# On Windows (PowerShell):
+
+
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# .\venv\Scripts\activate
+
+# Step 3: Install Django
+#  To install the latest version:
+
+# pip install django
+
+# pip install django
+# python -m django --version
+
+
+# Step 4: Freeze the Requirements (Optional)
+
+# To save the list of installed packages:
+
+# pip freeze > requirements.txt
