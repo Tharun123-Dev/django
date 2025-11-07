5390f12438d8f457aac0d3f95c24e003bbb2c8e3
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-11-07 15:38:33 +0530

    new

--- myproject1/myproject/settings.py
+++ myproject1/myproject/settings.py
@@ -72,11 +72,18 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "django",
+        "USER": "root",
+        "PASSWORD":"Nani@123",
+        "HOST":"127.0.0.1",
+        "PORT":"3306",
+        "OPTIONS":{'charset':'utf8mb4',
+                   'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"}
+
+
     }
 }
 
