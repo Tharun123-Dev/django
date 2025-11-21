1b5b6451c567aaba27dcd5b799ea8a79d54df92e
Tharun123-Dev <anjali.tharun9949@gmail.com>
2025-11-21 19:26:23 +0530

    db

--- myproject1/myproject/settings.py
+++ myproject1/myproject/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "django",
         "USER":'root',
-         'HOST':"localhost",
+        
          'PASSWORD':'Nani@123',
          "PORT":"3306",
          "OPTIONS":{
